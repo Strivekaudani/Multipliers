85e0773278611b907e1e5992d65245cbc6cdcda9
Rondrick Green <G44406939@gwu.edu>
2024-02-29 14:13:39 -0500

    Tested my modelsim scripts and simulation. Fixed a few issues.

--- simulation/scripts/design_files.sh
+++ simulation/scripts/design_files.sh
@@ -5,7 +5,7 @@
 design_files=("../../source/mult_simple.v" "../../source/mult_pipeline.v" "../../source/mult_booth.v")
 
 
-testbench_files=("../testbench/mult_pipeline_tb.v" "../testbench/mult_simple.v" "../testbench/mult_booth.v")
+testbench_files=("../testbench/mult_pipeline_tb.v" "../testbench/mult_simple_tb.v" "../testbench/mult_booth_tb.v")
 
 ver_files=("${design_files[@]}" "${testbench_files[@]}")
 
--- source/mult_pipeline.v
+++ source/mult_pipeline.v
@@ -5,30 +5,131 @@
 module mult_pipeline(
 	input wire clk,
 	input wire rst_n,
+    input wire start,
+    output wire valid,
 	input wire signed [3:0] a,
 	input wire signed [3:0] b,
-	output reg signed [7:0] c
+	output wire signed [7:0] c
 	);
+
+   reg [1:0] state_current;
+   reg [1:0] state_next;
 
    reg [7:0] multiplicand;
    reg [15:0] product;
-   
+   reg       ready;
+   reg       ready_next;
+   reg [7:0] partial_product;
+   reg [2:0] bit_counter; // Cycle Counter
+   reg [2:0] bit_counter_next;
+   reg [7:0] result;
+   reg [7:0] result_next;
 
    // decalare state names
    parameter [1:0]   IDLE     = 2'd0;
    parameter [1:0]   MULTIPLY = 2'd1;
-   parameter [1:0]   OUTPUT   = 2'd2;
 
+///////////////////////////////////////////////////////////////////////////////
+// State Control Section
+
+// State Control Sequential
 always @(posedge clk or negedge rst_n)
 	begin
 		if (rst_n == 1'b0) begin
-			c <= 8'h00;
+            state_current <= IDLE;
+            bit_counter   <= 0;
 		end else begin
-			multiplicand <= { 4{b[3]}, {b[3:0]}};
-			product	     <= { 8'b0, { 4{a[3]}, {a[3:0]}};
+            state_current <= state_next;
+            bit_counter   <= bit_counter_next;
 		end
 	end
 
+// State Control Combinational
+always @(*)
+begin
+    case (state_current)
+        IDLE:
+        begin
+            state_next = start ? MULTIPLY : IDLE;
+            bit_counter_next = 0;
+        end
 
+        MULTIPLY :
+        begin
+            state_next = (bit_counter == 7) ? IDLE : MULTIPLY;
+            bit_counter_next = bit_counter + 1;
+        end
+
+        default :
+        begin
+            state_next = IDLE;
+            bit_counter_next = 0;
+        end
+    endcase
+end
+
+///////////////////////////////////////////////////////////////////////////////
+
+// Register capture
+always @(posedge clk or negedge rst_n)
+begin
+    if (rst_n == 0) begin
+        product      <= 0;
+        multiplicand <= 0;
+        ready        <= 0;
+        result       <= 0;
+    end else begin
+
+        ready  <= ready_next;
+        result <= result_next;
+
+        case (state_current)
+            IDLE : begin
+                multiplicand <= { {4{b[3]}}, {b[3:0]} };        // Register input(s), Sign Extend
+                product      <= { 8'b0, {4{a[3]}}, {a[3:0]}};
+            end // IDLE
+
+            MULTIPLY : begin
+                product [15]   <= 0;                            // Roll the shift register
+                product [14:7] <= partial_product;
+                product [6:0]  <= product[7:1];
+            end // MULTIPLY
+
+            default : begin
+                multiplicand <= 0;
+                product      <= 0;
+            end
+
+        endcase
+    end
+end // Always Sequential
+
+
+// Combinational Logic for Multiplier
+always @(*)
+begin
+    // Add the multiplicand
+    if (product[0] == 1'b1) begin
+        partial_product = product[15:8] + multiplicand;         // Perform the addtion
+    end else begin
+        partial_product = product[15:8];
+    end
+
+    if (bit_counter == 7) begin                                 // For an 8-bit output capture
+        ready_next  = 1'b1;
+    end else begin
+        ready_next  = 1'b0;
+    end
+
+    result_next = ready ? product[7:0] : result;
+
+end // Always Combinational
+
+
+// Output Block
+assign c = ready ? product[7:0] : result;
+assign valid = ready;
+
+///////////////////////////////////////////////////////////////////////////////
 
 endmodule
